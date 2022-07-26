c00a4721e4bff2fc3f7a05b397a262193346fa48
Sergio Herranz <sergioherranzm@gmail.com>
2022-07-26 21:07:43 +0200

    fix - Rock.png

--- src/actors/MapBlockClasses.ts
+++ src/actors/MapBlockClasses.ts
@@ -2,7 +2,7 @@ import { Point } from '../types/Point';
 import { MapBlock } from './MapBlock';
 import sprite_destructible_1 from '../assets/tiles/Crate_entera.png'
 import sprite_destructible_2 from '../assets/tiles/Crate_rota.png'
-import sprite_notDestructible from '../assets/tiles/rock.png'
+import sprite_notDestructible from '../assets/tiles/Rock.png'
 import { actors } from '../script';
 
 export class DestructibleBlock extends MapBlock {
